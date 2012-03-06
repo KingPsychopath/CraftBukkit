bee90580a51ca9af291f817042c1553d8863266c
EvilSeph <evilseph@gmail.com>
2012-03-05 22:44:29 -0500

    Fixed the dreaded GSOD crash. Fixes BUKKIT-1040

--- src/main/java/net/minecraft/server/NetServerHandler.java
+++ src/main/java/net/minecraft/server/NetServerHandler.java
@@ -257,7 +257,7 @@ public class NetServerHandler extends NetHandler implements ICommandListener {
 
                     if (packet10flying.hasPos && packet10flying.y == -999.0D && packet10flying.stance == -999.0D) {
                         // CraftBukkit start
-                        if (packet10flying.x > 1 || packet10flying.z > 1) {
+                        if (Math.abs(packet10flying.x) > 1 || Math.abs(packet10flying.z) > 1) {
                             System.err.println(player.getName() + " was caught trying to crash the server with an invalid position.");
                             player.kickPlayer("Nope!");
                             return;
